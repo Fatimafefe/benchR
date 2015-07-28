6db0e13229e58f9db48ae88508a349b34dbf61fa
helenaK <helena.kotthaus@tu-dortmund.de>
2015-07-28 14:51:01 +0200

    Update bin_packing.R

--- Misc/bin_packing.R
+++ Misc/bin_packing.R
@@ -1,6 +1,12 @@
 # ------------------------------------------------------------------
-# Contributed by Michel Lang, TU Dortmund
-# ------------------------------------------------------------------
+# This material is distributed under the GNU General Public License
+# Version 2. You may review the terms of this license at
+# http://www.gnu.org/licenses/gpl-2.0.html
+#
+# Copyright (c) 2012-2016, Michel Lang, Helena Kotthaus,
+# TU Dortmund University
+#
+# All rights reserved.
 #
 # Simple greedy bin packing algorithm.
 # Same capacity for all bins. Number of bins auto-determined.
