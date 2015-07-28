6d9e093249c935db88d794668ddf52d742191b84
helenaK <helena.kotthaus@tu-dortmund.de>
2015-07-28 14:44:30 +0200

    Update MLPkgIns.R

--- MachineLearningAlg/mlrUCI/MLPkgIns.R
+++ MachineLearningAlg/mlrUCI/MLPkgIns.R
@@ -3,7 +3,7 @@
 # Version 2. You may review the terms of this license at
 # http://www.gnu.org/licenses/gpl-2.0.html
 #
-# Copyright (c) 2012-2014, Michel Lang, Helena Kotthaus,
+# Copyright (c) 2012-2016, Michel Lang, Helena Kotthaus,
 # TU Dortmund University
 #
 # All rights reserved.
